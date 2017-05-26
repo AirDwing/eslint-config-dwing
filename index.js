5dc7d786a79a9308e7f97f296dfad7299e2e4a25
willin <willin@willin.org>
2017-05-26 10:48:00 +0800

    newline:0

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ module.exports = {
     'comma-dangle': [2, 'never'],
     'consistent-return': 1,
     'max-len': [1, 150, 2],
+    'newline-per-chained-call': 0,
     'no-console': 1,
-    "no-mixed-operators": [2, {
-      "groups": [["&", "|", "^", "~", "<<", ">>", ">>>"], ["&&", "||"]],
-      "allowSamePrecedence": true
+    'no-mixed-operators': [2, {
+      groups: [['&', '|', '^', '~', '<<', '>>', '>>>'], ['&&', '||']],
+      allowSamePrecedence: true
     }],
     'no-param-reassign': [1, { props: false }],
     'no-underscore-dangle': [2, { allowAfterThis: true, allowAfterSuper: false }],
